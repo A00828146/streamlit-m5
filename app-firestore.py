fb2890c6c7b5fa5e0cee6341a38243ef5d93c803
Christian Terron <75289330+A00828146@users.noreply.github.com>
2023-03-06 15:21:41 +0000

    Deploy Prod

--- app-firestore.py
+++ app-firestore.py
@@ -6,9 +6,9 @@ import json
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-# db = firestore.Client(credentials=creds, project="names-project-Christian")
+db = firestore.Client(credentials=creds, project="names-project-Christian")
 
-db = firestore.Client.from_service_account_json("christianFirebase.json")
+# db = firestore.Client.from_service_account_json("christianFirebase.json")
 dbNames = db.collection("newNames")
 
 
